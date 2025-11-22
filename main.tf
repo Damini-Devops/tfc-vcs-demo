031b7283ab658c64c8606397137183d42a9012ec
damini.s@finology.in <damini.s@finology.in>
2025-11-22 22:10:41 +0530

    First Commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  cloud {
+    organization = "Damini-Demo"
+    workspaces {
+      name = "damini-tfc-demo"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_s3_bucket" "demo" {
+  bucket = "tfc-cli-demo-damini-bucket-123"
+}
